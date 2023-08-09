825b8e876d9041c643da248f9100d5ef72946393
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-08-09 23:11:41 +0300

    add validation in index js

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const moviesRouter = require('./movies');
 const { login, register } = require('../controllers/users');
 const { auth } = require('../middlewares/auth');
 const NotFoundError = require('../helpers/errors/NotFoundError');
+const { registerValidation, loginValidation } = require('../middlewares/validation');
 
 // роуты без авторизации
-router.post('/signin', login);
-router.post('/signup', register);
+router.post('/signin', loginValidation, login);
+router.post('/signup', registerValidation, register);
 // роуты, требующие авторизацию
 router.use(auth);
 router.use('/users', usersRouter);
